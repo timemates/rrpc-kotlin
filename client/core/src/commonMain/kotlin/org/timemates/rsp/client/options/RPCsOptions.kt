115ba08e824819b8aa35ec544f29563145a699b0
y9vad9 <y9vad9@gmail.com>
2024-08-19 20:29:27 +0200

    chore&feat: add EMPTY instance in a companion object, constructor with pairs for convenience

--- client/core/src/commonMain/kotlin/org/timemates/rsp/client/options/RPCsOptions.kt
+++ client/core/src/commonMain/kotlin/org/timemates/rsp/client/options/RPCsOptions.kt
@@ -6,6 +6,12 @@ import org.timemates.rsp.options.Options
 public value class RPCsOptions(
     private val map: Map<String, Options>
 ) {
+    public companion object {
+        public val EMPTY: RPCsOptions = RPCsOptions(emptyMap())
+    }
+
+    public constructor(vararg pairs: Pair<String, Options>) : this(mapOf(*pairs))
+
     public operator fun get(name: String): Options? = map[name]
 }
 
